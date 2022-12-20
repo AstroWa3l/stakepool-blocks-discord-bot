1ded216a134b2dded942df8ecef3ba6fddfda2d1
Wael <wael.t.ivie@gmail.com>
2022-12-20 11:14:40 -0800

    add script/update readme

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # stakepool-blocks-discord-bot
-Bot for discord to track when your stake pool made a new block XD
+Another Bot for discord to track when your stake pool made a new block XD
+
+
+## How to use the bot
+
+1. Clone the repo on the server/computer you will use to run the bot 24/7
+
+2. Inside the stakepool-blocks-discord-bot folder create a environment variable file named `.env`
+
+3. Now edit the `.env` file using your preferred text editor and add the following lines
+
+```python
+POOL_ID='stake pool bech_32 ID'
+DISCORD_TOKEN='Discord Application Token'
+DISCORD_GUILD='Discord Guild/Server Name'
+CHANNEL_ID='Discord channel ID'
+```
+4. Now open your computer's terminal CLI and change to the folder and run the script by using following command.
+
+	```python3 block_bot.py```
---
+++ block_bot.py
@@ -0,0 +1,105 @@
+# bot.py
+from email import message
+import os
+from webbrowser import get
+import discord
+import pandas as pd
+from discord.ext import tasks
+from dotenv import load_dotenv
+load_dotenv()
+import koios_python
+import requests
+
+TOKEN = os.getenv('TEST_DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+POOL = os.getenv('POOL_ID')
+CHANNEL_ID = os.getenv('CHANNEL_ID')
+
+# Create Koios Object 
+# Not working consistently so using my own function for latest blocks for now
+kp = koios_python.URLs()
+
+# Look up pool info
+pool_info = kp.get_pool_info(POOL)
+ticker = pool_info[0]['meta_json']['ticker']
+
+# Get the latest blocks list
+def get_block_list(content_range="0-999"):
+        
+        headers = {'Range': content_range}
+        reqs = requests.get('https://api.koios.rest/api/v0/blocks', headers=headers)
+        print(reqs.status_code)
+        # add try/except api error handling
+        try:
+                block_info = reqs.json()
+                pd_block_info = pd.DataFrame(block_info)
+                return pd_block_info
+        
+        except ApiError as e:
+                print(e)
+        
+##################################################################################################
+
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+
+@tasks.loop(seconds=22)
+async def main():
+        
+        channel = client.get_channel(int(CHANNEL_ID))
+        messages = [msg async for msg in channel.history(limit=1)]
+        contents = [message.content for message in messages]
+        
+        
+        # Get Data of last 3 blocks every few seconds
+        # latest_5_blocks = kp.get_blocks(content_range="0-2")
+        latest_5_blocks = get_block_list(content_range="0-2")
+        latest_5_blocks = pd.DataFrame(latest_5_blocks)
+
+        
+        if type(latest_5_blocks) == type(pd.DataFrame()):
+                
+                if len(latest_5_blocks) > 0:
+                        print("Latest Block Hash: {}\nBlock Height No: {}\nMade By Pool: {}"
+                              .format(latest_5_blocks.hash[0],latest_5_blocks.block_height[0],latest_5_blocks.pool[0]))
+                
+                        for block in range(len(latest_5_blocks)):
+                                if latest_5_blocks.pool[block] == POOL:
+                                        print("AHOY!")
+                                        
+                                        # ticker = armada_pools_df[armada_pools_df['POOL_bech32'] == latest_5_blocks.pool[block]].ticker.values[0]
+                                        # POOL_hex = armada_pools_df[armada_pools_df['POOL_bech32'] == latest_5_blocks.pool[block]].POOL_hex.values[0] 
+                                        
+                                        message="""
+                                        **Ahoy! More Plunder** 🏴‍☠️
+                                        \n**New Block** 🧱 **added to** ***{}*** **pool's treasure chest**💰
+                                        \n🪪 **Pool ID:** ***{}***
+                                        \n#️⃣ **Hash:** ***{}***
+                                        \n🕰 **Epoch:** ***{}***   🔢**Height_No:** ***{}***
+                                        \n📏 **Size**: ***{}*** kB  🔢**Number of Tx:** ***{}***
+                                        \n⚙️ **Total Blocks:** ***{}***
+                                        \n🧱**Info:** https://cexplorer.io/block/{}
+                                        """.format(ticker,
+                                                   latest_5_blocks.pool[block],
+                                                   latest_5_blocks.hash[block],
+                                                   latest_5_blocks.epoch_no[block],
+                                                   latest_5_blocks.block_height[block],
+                                                   round(latest_5_blocks.block_size[block].astype(int)/1000, 2),
+                                                   latest_5_blocks.tx_count[block],
+                                                   pool_info[0]['block_count'],
+                                                   latest_5_blocks.hash[block],
+                                                   )
+                                        for i in contents:
+                                                if i.__contains__(latest_5_blocks.hash[block]) == False:
+                                                        await channel.send(message)
+                                                        print("Discord Message Sent")
+                                                        
+
+@client.event
+async def on_ready():
+        if not main.is_running():
+                main.start()
+
+
+
+client.run(TOKEN)
